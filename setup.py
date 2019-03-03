e38db5c91bc8220fe786468065306a1bc1724d9f
Konrad Zdeb <konrad.zdeb@me.com>
2019-03-03 17:21:44 +0100

    Installation
    Added very basic version of setup.py. file
    
    Signed-off-by: Konrad Zdeb <konrad.zdeb@me.com>

---
+++ setup.py
@@ -0,0 +1,16 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+#
+# Copyright © 2019 Konrad <konrad.zdeb@me.com>
+#
+# Distributed under terms of the GPL 3-0 license.
+
+"""
+Setup script for SPARQL query runner.
+"""
+
+###########
+# Modules #
+###########
+from distutils.core import setup
